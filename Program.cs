1a6500822cef532ee9223c736d18d90a6417f6c5
matisagra <sagramatias04@gmail.com>
2024-05-28 16:59:28 -0300

    carga de pendientes

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Distribuidora;
+
+Console.WriteLine("Ingrese la cantidad de tareas: ");
+string cantidad=Console.ReadLine();
+int cant;
+int.TryParse(cantidad, out cant);
+
+Tarea[] tareasPendientes = new Tarea[cant];
+
+int Pendientes=0;
+
+for (int i = 0; i < cant; i++)
+{
+    int TareaID = Pendientes + 1;
+    string Descripcion = "blabla";
+    int Duracion =  new Random().Next(10, 100);
+    tareasPendientes[i] = new Tarea(TareaID, Descripcion, Duracion);
+    Pendientes++;
+}
+
+foreach (Tarea tareas in tareasPendientes)
+{
+    Console.WriteLine(tareas.TareaID);
+     Console.WriteLine(tareas.Descripcion);
+      Console.WriteLine(tareas.Duracion);
+}
+
---
+++ distribuidora.cs
@@ -0,0 +1,18 @@
+namespace Distribuidora{
+public class Tarea{
+    public int TareaID;
+    public string Descripcion;
+    public int Duracion;
+
+    public Tarea(int id, string descripcion, int duracion){
+        TareaID = id;
+        Descripcion = descripcion;
+        Duracion = duracion;
+    }
+
+    
+
+
+}
+
+}
