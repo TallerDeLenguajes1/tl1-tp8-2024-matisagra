bbbd52e66b3bbf3cd6556ab2810b3580c170b4aa
matisagra <sagramatias04@gmail.com>
2024-05-28 17:59:09 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,27 +1,55 @@
 ﻿using Distribuidora;
 
 Console.WriteLine("Ingrese la cantidad de tareas: ");
-string cantidad=Console.ReadLine();
+string cantidad = Console.ReadLine();
 int cant;
 int.TryParse(cantidad, out cant);
 
-Tarea[] tareasPendientes = new Tarea[cant];
+List<Tarea> tareasPendientes = new List<Tarea>();
+List<Tarea> tareasRealizadas = new List<Tarea>();
 
-int Pendientes=0;
+// carga aleatoria
+int indicePendientes = 0;
 
 for (int i = 0; i < cant; i++)
 {
-    int TareaID = Pendientes + 1;
+    int TareaID = indicePendientes + 1;
     string Descripcion = "blabla";
-    int Duracion =  new Random().Next(10, 100);
-    tareasPendientes[i] = new Tarea(TareaID, Descripcion, Duracion);
-    Pendientes++;
+    int Duracion = new Random().Next(10, 100);
+    Tarea tarea = new Tarea(TareaID, Descripcion, Duracion);
+    tareasPendientes.Add(tarea);
+    indicePendientes++;
 }
 
-foreach (Tarea tareas in tareasPendientes)
+// mostrar pendientes
+foreach (var tareas in tareasPendientes)
 {
+
     Console.WriteLine(tareas.TareaID);
-     Console.WriteLine(tareas.Descripcion);
-      Console.WriteLine(tareas.Duracion);
+    Console.WriteLine(tareas.Descripcion);
+    Console.WriteLine(tareas.Duracion);
 }
 
+// mover realizadas
+Console.WriteLine("Ingrese el id de la tarea que desea eliminar");
+string ingresar = Console.ReadLine();
+int id;
+int.TryParse(ingresar, out id);
+
+Tarea buscado = tareasPendientes.Find(t => t.TareaID == id);
+
+if (buscado != null)
+{
+    tareasPendientes.Remove(buscado);
+    tareasRealizadas.Add(buscado);
+    Console.WriteLine("Tarea Transferida");
+}
+
+//mostrar realizadas
+foreach (var tareas in tareasRealizadas)
+{
+
+    Console.WriteLine(tareas.TareaID);
+    Console.WriteLine(tareas.Descripcion);
+    Console.WriteLine(tareas.Duracion);
+}
--- distribuidora.cs
+++ distribuidora.cs
@@ -1,18 +1,18 @@
-namespace Distribuidora{
-public class Tarea{
-    public int TareaID;
-    public string Descripcion;
-    public int Duracion;
+namespace Distribuidora
+{
+    public class Tarea
+    {
+        public int TareaID;
+        public string Descripcion;
+        public int Duracion;
 
-    public Tarea(int id, string descripcion, int duracion){
-        TareaID = id;
-        Descripcion = descripcion;
-        Duracion = duracion;
+        public Tarea(int id, string descripcion, int duracion)
+        {
+            TareaID = id;
+            Descripcion = descripcion;
+            Duracion = duracion;
+        }
+
     }
-
-    
-
-
-}
 
 }
